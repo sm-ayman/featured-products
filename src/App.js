b06b3919e9fb0ecce777759edb57f97511391c05
sm_ayman <sultanmdayman@gmail.com>
2025-05-17 01:39:33 +0600

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 // import Products from "./components/Products";
 import Heading from "./components/Heading";
 import ProductList from "./components/ProductList";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const App = () => {
   return (
     <div>
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Heading = () => {
   return (
-    <div>
+    <div className="text-center mt-3">
       <h1>Featured Product</h1>
       <p>Check & Get Your Desired Product!</p>
     </div>
